ed74d4687375bb5b7d4fbf1947a2a2df5e2726eb
Steffen Matthischke <steffen.matthischke@gmail.com>
2016-09-25 17:29:54 +0200

    Initial Commit

---
+++ Abfahrtmonitor/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Abfahrtmonitor
+//
+//  Created by Steffen Matthischke on 23.09.16.
+//  Copyright © 2016 Matthischke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
